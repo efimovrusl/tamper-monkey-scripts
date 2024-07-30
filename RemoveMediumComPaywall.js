f5ece232897f0ecc8d5b87ab3fa2fa4d439a003c
yefimov.ruslan <57012004+efimovrusl@users.noreply.github.com>
2024-07-30 14:44:32 +0100

    Update RemoveMediumComPaywall.js
    Not it opens another tab, instead of redirecting because sometimes freedium doesn't work.

--- RemoveMediumComPaywall.js
+++ RemoveMediumComPaywall.js
@@ -1,14 +1,29 @@
 // ==UserScript==
-// @name         Remove medium.com paywall without a subscription
+// @name         Auto-open medium.com articles without paywall in new tab
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  try to take over the world!
+// @version      1.2
+// @description  Automatically opens medium.com articles in a new tab using freedium.cfd
 // @author       You
 // @match        https://*.medium.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-(function () {
-    window.location.href = window.location.href.replace(/https:\/\/([^.]*\.)?medium\.com/g, "https://freedium.cfd/");
+(function() {
+    'use strict';
+
+    // Function to check if the current page is an article
+    function isArticlePage() {
+        // This is a basic check. You might need to adjust it based on Medium's URL structure
+        return window.location.pathname.split('/').length > 2;
+    }
+
+    // Only proceed if this is an article page
+    if (isArticlePage()) {
+        // Generate the new URL
+        var newUrl = window.location.href.replace(/https:\/\/([^.]*\.)?medium\.com/g, "https://freedium.cfd/");
+
+        // Open the new URL in a new tab
+        window.open(newUrl, '_blank');
+    }
 })();
